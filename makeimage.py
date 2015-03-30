b2778f59598cc0f6b18ea25c52874a31e7cd222d
Mohamed Saleem <mohsal.saleem@gmail.com>
2015-03-31 00:52:00 +0530

    @nd

--- makeimage.py
+++ makeimage.py
@@ -3,36 +3,25 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.new("RGBA",(3508,4961),(0,0,0))
-img.save("f.png")
-
-img = Image.open('f.png')
-img = img.convert("RGBA")
-datas = img.getdata()
-
-newData = []
-for item in datas:
-    if item[0] == 255 and item[1] == 255 and item[2] == 255:
-        newData.append((255, 255, 255, 0))
-    else:
-        newData.append(item)
-
-img.putdata(newData)
-img.save("img2.png", "PNG")
-
+img = Image.new("RGB",(3508,4961),(255,255,255))
 print "Enter starting number"
 start = input()
 
 k = start
 
-background = Image.open("img2.png")
-kid = "KQ16"+str(k).zfill(4)
-foreground = Image.open(kid+".png")
-
+#kid = "KQ16"+str(k).zfill(4)
+#foreground = Image.open(kid+".png")
+"""
+kid = []
+for i in range(16):
+	kid.append([])
+	for j in range(10):
+		kid[i].append("KQ16"+str(i*10+j+k).zfill(4)+".png") 
+print kid
+"""
+count = 0
 for i in range(10):
 	for j in range(16):
-		background.paste(foreground,(i*302,j*302),foreground)
-
-
-#background.paste(foreground,(0*300,2*300),foreground)
-background.save("final.png")
+		img.paste(Image.open("KQ16"+str(j*10+i+k).zfill(4)+".png"),(i*350,j*302))
+		#print "KQ16"+str(i*10+j+k).zfill(4)+".png"
+img.save("final.png")
